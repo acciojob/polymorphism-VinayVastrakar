09edf3cf3035fd6cd8af599bdf38ac6f7e3de092
Vinay Vastrakar <Vinayvastrakar13@gmail.com>
2023-05-11 16:13:02 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,24 +1,33 @@
 package com.driver;
-import java.util.*;
-
 public class Main {
 
-    public static class Product{
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
 
-        public int product(int x,int y){
-            return x+y;
+        public int product(int x, int y, int z) {
+            return x * y * z;
         }
-        public int product(int x,int y, int z){
-            return x+y+z;
-        }
-        public double product(double x,double y){
-            return x+y;
+
+        public double product(double x, double y) {
+            return x * y;
         }
     }
+
     public static void main(String[] args) {
         Product p = new Product();
-        p.product(1, 2);
-        p.product(1, 2, 3);
-        p.product(1.1, 2.2);
+
+        // call the first method
+        int result1 = p.product(3, 4);
+        System.out.println("Result 1: " + result1);
+
+        // call the second method
+        int result2 = p.product(2, 5, 7);
+        System.out.println("Result 2: " + result2);
+
+        // call the third method
+        double result3 = p.product(1.5, 2.5);
+        System.out.println("Result 3: " + result3);
     }
 }
